399319873cd1a31deffce11afd313bcd9d04ee67
Manuel Arrabal <69895541+manuel-arrabal@users.noreply.github.com>
2025-12-30 16:19:41 +0100

    Create ui.js

---
+++ src/ui.js
@@ -0,0 +1,12 @@
+// Spanish strings for front-end
+const TEXTS = {
+  questionMostPopularName: "¿Cuál fue el nombre más popular en el año",
+  questionMostPopularYear: "¿En qué año fue más popular el nombre",
+  correct: "¡Correcto!",
+  incorrect: "Incorrecto",
+  answerWas: "El número de nacimientos fue",
+  score: "Puntuación: ",
+  playAgain: "Jugar otra vez",
+  totalAnswered: "Preguntas contestadas correctamente: ",
+  next: "Siguiente"
+};
